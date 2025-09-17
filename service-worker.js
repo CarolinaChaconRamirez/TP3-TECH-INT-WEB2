187c5573c85b52c2a9b910cdf5ff704cf0bd58ac
Carolina Chacon Ramirez <elizabethcarolina.chrz@gmail.com>
2025-09-17 15:57:16 -0400

    service worker
    ajout

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
 'offline.html',
+'index.html',
+'projet.html',
+'contact.html'
+
 ];
 self.addEventListener('install', (evt) => {
 console.log('[ServiceWorker] Install');
@@ -13,3 +17,43 @@ return cache.addAll(FILES_TO_CACHE);
 );
 self.skipWaiting();
 });
+
+//Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v2';
+self.addEventListener('activate', (evt) => {
+console.log('[ServiceWorker] Activate');
+//Remove previous cached data from disk.
+evt.waitUntil(
+caches.keys().then((keyList) => {
+return Promise.all(keyList.map((key) => {
+if (key !== CACHE_NAME) {
+console.log('[ServiceWorker] Removing old cache',
+key);
+return caches.delete(key);
+}
+}));
+})
+);
+self.clients.claim();
+});
+
+// 
+
+self.addEventListener('fetch', (evt) => {
+console.log('[ServiceWorker] Fetch', evt.request.url);
+//Add fetch event handler here.
+if (evt.request.mode !== 'navigate') {
+// Not a page navigation, bail.
+return;
+}
+evt.respondWith(
+fetch(evt.request)
+.catch(() => {
+return caches.open(CACHE_NAME)
+.then((cache) => {
+return cache.match('/TP3-TECH-INT-WEB2/PortfoliodeCarolina/offline.html') ;
+});
+})
+);
+});
+
