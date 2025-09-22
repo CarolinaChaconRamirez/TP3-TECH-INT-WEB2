6d719bdd125acece052a9e8691c028d0de374eec
Carolina Chacon Ramirez <elizabethcarolina.chrz@gmail.com>
2025-09-22 17:04:27 -0400

    suppression de img
    delete image non utilisees

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,49 @@
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = "static-cache-v2";
 const FILES_TO_CACHE = [
-  'index.html',
-  'projet.html',
-  'contact.html',
-  'projetlver.html'
+  "index.html",
+  "projet.html",
+  "contact.html",
+  "projetlver.html",
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", (evt) => {
+  console.log("[ServiceWorker] Install");
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
+      console.log("[ServiceWorker] Pre-caching offline page");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener("activate", (evt) => {
+  console.log("[ServiceWorker] Activate");
   evt.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  if (evt.request.mode !== 'navigate') {
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+  if (evt.request.mode !== "navigate") {
     return;
   }
   evt.respondWith(
-    fetch(evt.request)
-      .catch(() => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          return cache.match(evt.request);
-        });
-      })
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request);
+      });
+    })
   );
 });
